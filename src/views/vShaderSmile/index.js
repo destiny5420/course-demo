d4b39dbe4ec42c7095fe2ca232d04fe830c0d876
paper.mac <paper.hsiao@gmail.com>
2020-12-26 21:16:28 +0800

    [modify] 搬移shadertoy的程式碼到vue上

--- src/views/vShaderSmile/index.js
+++ src/views/vShaderSmile/index.js
@@ -1,11 +1,60 @@
-// import * as THREE from 'three';
+import * as THREE from 'three';
 
-function init() {
-  console.log('*** [shader smile] init ***');
+let container;
+let camera;
+let scene;
+let renderer;
+let uniforms;
+
+function onWindowResize() {
+  renderer.setSize(window.innerWidth - window.innerWidth * 0.15, window.innerHeight);
+  uniforms.u_resolution.value.x = renderer.domElement.width;
+  uniforms.u_resolution.value.y = renderer.domElement.height;
+}
+
+function render() {
+  uniforms.u_time.value += 0.05;
+  renderer.render(scene, camera);
 }
 
 function animate() {
-  console.log('*** [shader smile] animate ***');
+  // console.log('*** [closure] [shader smile] animate ***');
+  requestAnimationFrame(animate);
+  render();
+}
+
+function init() {
+  console.log('*** [closure] [shader smile] init ***');
+
+  container = document.getElementById('shader-smile-main');
+
+  camera = new THREE.Camera();
+  camera.position.z = 1;
+
+  scene = new THREE.Scene();
+
+  const geometry = new THREE.PlaneBufferGeometry(2, 2);
+
+  uniforms = {
+    u_time: { type: 'f', value: 1.0 },
+    u_resolution: { type: 'v2', value: new THREE.Vector2() },
+  };
+
+  const material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: document.getElementById('vertexshader').textContent,
+    fragmentShader: document.getElementById('fragmentshader').textContent,
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+  onWindowResize();
+  window.addEventListener('resize', onWindowResize, false);
 }
 
 export default {
@@ -13,7 +62,9 @@ export default {
   props: {},
   components: {},
   data: function() {
-    return {};
+    return {
+      shaderSmileData: null,
+    };
   },
   methods: {},
   computed: {},
